3306ce96ad629dca4df9cdf38531d0892b7a8ed6
NikolaiX <nik.allen101@gmail.com>
2016-02-18 16:15:10 +1300

    Application Test, updated gradle and added Google API key to manifest

--- app/src/androidTest/java/com/metalplastic/meetme/ApplicationTest.java
+++ app/src/androidTest/java/com/metalplastic/meetme/ApplicationTest.java
@@ -3,6 +3,14 @@ package com.metalplastic.meetme;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
+import com.metalplastic.meetme.Interfaces.IWebMapProvider;
+import com.metalplastic.meetme.MapServices.MapServiceGoogle;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
